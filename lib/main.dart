efc0ddf92cdb719ec8ad70bc862894ba948afd6d
Talha-glitched <m.talha2703@gmail.com>
2024-01-31 12:36:31 +0500

    Login and NAvigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/routes.dart';
 import '/pages/home_page.dart';
 import 'pages/login_page.dart';
 import 'package:google_fonts/google_fonts.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -10,16 +12,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      themeMode: ThemeMode.light,
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-      ),
+        primarySwatch: Colors.deepPurple
+        ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
-      ),
+      ),  
       routes: {
-        "/": (context) => LoginPage(),
-        "/home": (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,55 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Material(
-        child: Column(
-      children: [
-        Image.asset(
-          "assets/images/login.png",
-          fit: BoxFit.cover,
-        ),
-        SizedBox(
-          height: 20.0,
-        ),
-        Text(
-          "Welcome",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/login.png",
+            fit: BoxFit.cover,
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32),
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                    hintText: "Enter Username", labelText: "Username"),
-              ),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                    hintText: "Enter Password", labelText: "Password"),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              ElevatedButton(
-                child: Text("Login"),
-                style: TextButton.styleFrom(foregroundColor: Colors.black,
-                backgroundColor: Colors.blueAccent),
-                onPressed: () {
-                  print("Hi Talha");
-                },
-              )
-            ],
+          SizedBox(
+            height: 20.0,
           ),
-        )
-      ],
+          Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Enter Username", labelText: "Username"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter Password", labelText: "Password"),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                ElevatedButton(
+                  child: Text("Login"),
+                  style: TextButton.styleFrom(
+                      minimumSize: Size(150, 40),
+                      foregroundColor: Colors.black,
+                      backgroundColor: Colors.blueAccent),
+                  onPressed: () {
+                    Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  },
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     ));
   }
 }
